5365eb7bed389119796b383ae1de8100665b18c7
hasancafarov <hasan215hh@gmail.com>
2022-09-11 02:23:46 +0400

    Time: 141 ms (99.41%), Space: 44.6 MB (8.68%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+         Dictionary<int,int> dict=new Dictionary<int,int>();
+        for(int i=0;i<nums.Length;i++){
+            if(dict.ContainsKey(target-nums[i])){
+                return new int[]{dict[target-nums[i]],i};
+            }
+            dict[nums[i]]=i;
+        }
+        return new int[]{};
+    }
+}
