9cf9fac0a1a05ef0946cd52138c34f863df4f244
hasancafarov <hasan215hh@gmail.com>
2022-09-11 02:57:19 +0400

    Time: 36 ms (50.83%), Space: 25.6 MB (28.13%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int UniquePaths(int m, int n) {
+         int N = n + m - 2;
+            int r = m - 1; 
+            double res = 1;
+            
+            for (int i = 1; i <= r; i++)
+                res = res * (N - r + i) / i;
+            return (int)res;
+    }
+}
