c0ae02b1803b7ae398dcbe00f561b3949f22a1ad
hasancafarov <hasan215hh@gmail.com>
2022-09-11 20:05:06 +0400

    Time: 138 ms (35.62%), Space: 37.4 MB (97.42%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+     int lo = 0;
+    int hi = nums.Length - 1;
+    while (lo < hi) {
+        int mid = (lo + hi) / 2;
+        if (nums[mid] == target) return mid;
+        
+        if (nums[lo] <= nums[mid]) {
+            if (target >= nums[lo] && target < nums[mid]) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        } else {
+            if (target > nums[mid] && target <= nums[hi]) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+    }
+    return nums[lo] == target ? lo : -1;
+    }
+}
